46a5f25214db3a907cc308b9f511a5f8bcba1e32
Владимир Матасов <dragon111@mail.ru>
2024-03-13 00:51:45 +0300

    Correct model "User" and add model "Subscribe"

--- backend/users/models.py
+++ backend/users/models.py
@@ -11,6 +11,13 @@ MAX_LENGTH_LASTNAME = 150
 MAX_LENGTH_PASSWORD = 150
 LENGTH_CONFIRMATION_CODE = 150
 
+AUTHOR_AND_USER_IS_NOT_UNIQUE = (
+    'Пользователь и автор рецепта не могут повторяться! '
+)
+AUTHOR_NOT_SIGNER_TO_AUTHOR = (
+    'Автор рецепта не может подписываться сам на себя!'
+)
+
 
 class User(AbstractUser):
     """Класс кастомного пользователя."""
@@ -19,32 +26,35 @@ class User(AbstractUser):
         verbose_name='Никнейм',
         unique=True,
         max_length=MAX_LENGTH_USERNAME,
-        validators=(validate_username,)
+        validators=(validate_username,),
+        help_text='Введите никнейм'
     )
     email = models.EmailField(
         verbose_name='E-mail',
         unique=True,
         max_length=MAX_LENGTH_EMAIL,
+        help_text='Введите свой e-mail'
     )
     first_name = models.CharField(
         verbose_name='Имя',
         blank=True,
         max_length=MAX_LENGTH_FIRSTNAME,
+        help_text='Введите свое имя'
     )
     last_name = models.CharField(
         verbose_name='Фамилия',
         blank=True,
         max_length=MAX_LENGTH_LASTNAME,
+        help_text='Введите свою фамилию'
     )
     password = models.CharField(
         verbose_name='Пароль',
-#        blank=True,
         max_length=MAX_LENGTH_PASSWORD,
+        help_text='Введите пароль'
     )
-    is_subscribed = models.BooleanField(
-        verbose_name='Подписка',
-#        blank=True,
-    )
+#     is_subscribed = models.BooleanField(
+#         verbose_name='Подписка',
+#     )
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -56,4 +66,44 @@ class User(AbstractUser):
             f'Никнейм: {self.username[:20]}, '
             f'e-mail: {self.email}, '
             f'id: {self.pk}, '
+        )
+
+
+class Subscribe(models.Model):
+    """
+    Подписка на пользователей.
+    Комбинация автора рецепта и пользователя уникальна.
+    Автор не может подписываться на самого себя.
+    """
+
+    author = models.ForeignKey(
+        verbose_name='Подписка на автора рецепта',
+        related_name='signed',
+        help_text='Подписаться на автора рецепта'
+    )
+    user = models.ForeignKey(
+        verbose_name='Пользователь',
+        related_name='signer',
+        help_text='Текущий пользователь'
+    )
+
+    class Meta:
+        verbose_name = 'Мои подписки'
+        verbose_name_plural = 'Мои подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'ingredient'],
+                name='unique_recipe_ingredient',
+                message=AUTHOR_AND_USER_IS_NOT_UNIQUE
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('author')),
+                name='check_user_is_author',
+                message=AUTHOR_NOT_SIGNER_TO_AUTHOR
+            ),
+        ]
+
+    def __str__(self):
+        return (
+            f'Пользователь {self.user} подписан на {self.author}.'
         )
