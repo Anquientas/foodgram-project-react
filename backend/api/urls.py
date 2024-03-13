1fc8cf542a274dd038fdbe35b5609f2454263204
Владимир Матасов <dragon111@mail.ru>
2024-03-14 00:31:36 +0300

    Add first iteration urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,24 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    ...
+)
+
+
+router_v1 = SimpleRouter()
+router_v1.register('ingredients', ..., basename='ingredients')
+router_v1.register('recipes', ..., basename='recipes')
+router_v1.register('tags', ..., basename='tags')
+router_v1.register('users', ..., basename='users')
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/auth/', include(auth_urls))
+]
+
+
+
 
 urlpatterns = [
     (...),
