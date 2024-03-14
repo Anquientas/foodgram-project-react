6ceb82723ef613b961d87e64df8f31cfbb5a30dd
Владимир Матасов <dragon111@mail.ru>
2024-03-14 21:22:54 +0300

    Add application tags in admin-zone

--- backend/tags/admin.py
+++ backend/tags/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'color')
+    list_editable = ('name', 'slug', 'color')
+    search_fields = ('name', 'slug')
+    list_filter = ('name', 'slug', 'color')
+    list_display_links = ('id', 'name')
+
+
+admin.site.empty_value_display = 'Не задано'
+admin.site.register(Tag, TagAdmin)
