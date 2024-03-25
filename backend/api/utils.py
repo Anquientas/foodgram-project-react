eb0f2930466e3d479b248ad73d5dffe98c61e960
Владимир Матасов <dragon111@mail.ru>
2024-03-25 16:35:02 +0300

    Correct project files

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -7,13 +7,13 @@ from recipes.models import IngredientRecipe
 DOWNLOAD_FILENAME = 'shopping_list.txt'
 
 
-def shopping_cart_ingredients(self, request, user):
+def shopping_cart_ingredients(self, request):
     """
     Функция скачивания списка продуктов
     для выбранных рецептов пользователя.
     """
     sum_ingredients_in_recipes = IngredientRecipe.objects.filter(
-        recipe__shopping_cart__user=user
+        recipe__shopping_cart__user=request.user
     ).values(
         'ingredient__name',
         'ingredient__measurement_unit'
